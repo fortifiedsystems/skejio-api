b0dd77ecb18d6cc9faa4666a2a674d1543c1a265
jasonhawkharris <jasonhawkharris@gmail.com>
2020-10-24 15:12:10 -0700

    Completes Comment Schema

--- models/Comment.js
+++ models/Comment.js
@@ -2,7 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-
+    thread: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Thread',
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Artist' || 'Manager' || 'Teammate',
+    },
+    content: String,
+}, {
+    timestamps: true,
 })
 
 const Comment = mongoose.model('Comment', CommentSchema);
