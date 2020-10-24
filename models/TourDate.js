ca7a64da3532dd1d978b6d78e7a9dce4ba7bacb8
jasonhawkharris <jasonhawkharris@gmail.com>
2020-10-24 14:59:47 -0700

    Adds TourDate Model and exports to index.js

--- models/TourDate.js
+++ models/TourDate.js
@@ -2,7 +2,40 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TourDateSchema = new Schema({
-
+    date: {
+        type: Date,
+        required: true,
+    },
+    venue: {
+        type: String,
+        required: true,
+    },
+    artist: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Artist',
+    },
+    fee: Number,
+    deposit: Number,
+    depositReceived: Boolean,
+    contract: String,
+    contractSigned: Boolean,
+    paidInFull: Boolean,
+    threads: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Thread',
+    }],
+    todos: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Todo',
+    }],
+    promoter: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Promoter',
+    },
+    tour: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Tour',
+    },
 });
 
 const TourDate = mongoose.model('TourDate', TourDateSchema);
