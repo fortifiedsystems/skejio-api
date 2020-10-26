f91a13266fea378535b95e029b5c397a5a581758
jasonhawkharris <jasonhawkharris@gmail.com>
2020-10-26 13:57:13 -0700

    create route for tourdate now pushes id to appropriate tour

--- controllers/tourDate.js
+++ controllers/tourDate.js
@@ -3,24 +3,45 @@ const db = require('../models');
 const index = (req, res) => {
     db.TourDate.find({ 'tour': req.params.tourId }, (err, foundTourDates) => {
         if (err) console.log('Error at tourDate#index:', err);
-        if (!foundTourDates) res.status(500).json({
+        if (!foundTourDates) res.status(200).json({
             'message': 'No tour dates assigned to this tour.'
-        })
+        });
         res.status(200).json({
-            'tourDates': foundTourDates
-        })
-    })
+            'tourDates': foundTourDates,
+        });
+    });
 }
 
-const create = (req, res) => {
-    db.TourDate.create(req.body, (err, createdTourDate) => {
-        req.body.tour = req.params.tourId
-        if (err) console.log('Error at tourDate#create:', err);
-        res.status(201).json({ 'tourDate': createdTourDate })
+const show = (req, res) => {
+    db.TourDate.findById(req.params.showId, (err, foundTourDate) => {
+        if (err) console.log('Error at tourDate#show', err);
+        if (!foundTourDate) res.status(200).json({
+            'message': 'Tour date not found.',
+        });
+        res.status(200).json({
+            'tourDate': foundTourDate,
+        });
     });
+}
+
+const create = async (req, res) => {
+    try {
+        const tour = await db.Tour.findById(req.params.tourId);
+        db.TourDate.create(req.body, (err, createdTourDate) => {
+            if (err) console.log('Error at tourDate#create:', err);
+            req.body.tour = req.params.tourId;
+            tour.tourDates.push(createdTourDate);
+            tour.save();
+            res.status(201).json({ 'tourDate': createdTourDate })
+        });
+    } catch (err) {
+        console.log('Error at tourDate#create:', err);
+    }
+
 }
 
 module.exports = {
     create,
     index,
+    show,
 }
--- routes/tourDate.js
+++ routes/tourDate.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const ctrl = require('../controllers');
 
 router.get('/:tourId', ctrl.tourDate.index);
-// router.get('/:tourId/:showId', ctrl.tourDate.show);
+router.get('/:tourId/:showId', ctrl.tourDate.show);
 router.post('/:tourId', ctrl.tourDate.create);
 // router.put('/:tourId/:showId', ctrl.tourDate.update);
 // router.delete('/:tourId/:showId', ctrl.tourDate.destroy);
