c550dfbff7381883671d07d9ef277af6f7b04379
jasonhawkharris <jasonhawkharris@gmail.com>
2020-10-26 13:28:27 -0700

    adds index method to controller

--- controllers/tourDate.js
+++ controllers/tourDate.js
@@ -1,5 +1,17 @@
 const db = require('../models');
 
+const index = (req, res) => {
+    db.TourDate.find({ 'tour': req.params.tourId }, (err, foundTourDates) => {
+        if (err) console.log('Error at tourDate#index:', err);
+        if (!foundTourDates) res.status(500).json({
+            'message': 'No tour dates assigned to this tour.'
+        })
+        res.status(200).json({
+            'tourDates': foundTourDates
+        })
+    })
+}
+
 const create = (req, res) => {
     db.TourDate.create(req.body, (err, createdTourDate) => {
         req.body.tour = req.params.tourId
@@ -10,4 +22,5 @@ const create = (req, res) => {
 
 module.exports = {
     create,
+    index,
 }
--- routes/tourDate.js
+++ routes/tourDate.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers');
 
-// router.get('/:tourId', ctrl.tourDate.index);
+router.get('/:tourId', ctrl.tourDate.index);
 // router.get('/:tourId/:showId', ctrl.tourDate.show);
 router.post('/:tourId', ctrl.tourDate.create);
 // router.put('/:tourId/:showId', ctrl.tourDate.update);
