699358b082264288b2759507c509b2fbc6e72ec6
jasonhawkharris <jasonhawkharris@gmail.com>
2020-10-26 19:23:55 -0700

    adds threadIndex route to comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,5 +1,31 @@
 const db = require('../models');
 
+const index = (req, res) => {
+    db.Comment.find({}, (err, foundComments) => {
+        if (err) console.log('Error at comment#index:', err);
+        if (!foundComments) res.status(200).json({
+            'message': 'No comments on this thread.'
+        })
+
+        res.status(200).json({
+            'comments': foundComments
+        });
+    });
+}
+
+const threadIndex = (req, res) => {
+    db.Comment.find({ 'thread': req.params.threadId }, (err, foundComments) => {
+        if (err) console.log('Error at comments#threadIndex', err);
+        if (!foundComments) res.status(200).json({
+            'message': 'No threads exist for this date.'
+        });
+
+        res.status(200).json({
+            'comments': foundComments,
+        });
+    });
+}
+
 const create = async (req, res) => {
     try {
         req.body.thread = req.params.threadId;
@@ -16,9 +42,10 @@ const create = async (req, res) => {
     } catch (error) {
         console.log('Error at comments#create:', err);
     }
-
 }
 
 module.exports = {
     create,
+    index,
+    threadIndex,
 }
--- routes/comments.js
+++ routes/comments.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers');
 
-// router.get('/', ctrl.comments.index);
-// router.get('/thread/:threadId', ctrl.comments.threadIndex);
+router.get('/', ctrl.comments.index);
+router.get('/thread/:threadId', ctrl.comments.threadIndex);
 // router.get('/:id', ctrl.comments.show);
 router.post('/create/:threadId', ctrl.comments.create);
 // router.put('/:id', ctrl.comments.update);
