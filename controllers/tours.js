617c9912c2f950fe687aa1f3e3ee8cf4ececc0bf
jasonhawkharris <jasonhawkharris@gmail.com>
2020-11-19 14:10:17 -0800

    Add additional authorization level for tours.

--- controllers/tours.js
+++ controllers/tours.js
@@ -1,5 +1,15 @@
 const db = require('../models');
 
+/**
+ * @NOTE For the front end: 
+ * if the user is a manager or agent, form will need a drop down
+ * populated with artists registered to that agent or manager.
+ * if the user is an artist, form only need a name field.
+ * if the user is a teammate, They shouldn't even be able 
+ * to access the form.
+ * @param {*} req 
+ * @param {*} res 
+ */
 const create = async (req, res) => {
     try {
         const user = await db.User.findById(req.userId);
@@ -9,6 +19,12 @@ const create = async (req, res) => {
             return res.status(403).json({
                 msg: 'Teammates cannot create tours.',
             })
+        } else {
+            if (!user.artists.includes(req.body.artist)) {
+                return res.status(403).json({
+                    msg: 'Not authorized to add tours for this artist.'
+                });
+            }
         }
 
         db.Tour.create(req.body, async (err, newTour) => {
