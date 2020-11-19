e4ae958309c3303e529fe288ef7935fb81320263
jasonhawkharris <jasonhawkharris@gmail.com>
2020-11-19 08:34:12 -0800

    deletes all controllers and routes to start over with new models.

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,4 @@
 module.exports = {
     auth: require('./auth'),
-    tours: require('./tours'),
-    tourDate: require('./tourDate'),
-    threads: require('./threads'),
-    comments: require('./comments'),
-    todos: require('./todos'),
     user: require('./user'),
 }
