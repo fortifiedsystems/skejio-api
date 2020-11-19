0264a7d79a29b47c77f0ae355527f2efa0c042a4
jasonhawkharris <jasonhawkharris@gmail.com>
2020-11-19 12:41:47 -0800

    Adds agencies

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,5 @@ module.exports = {
     auth: require('./auth'),
     user: require('./user'),
     companies: require('./companies'),
+    agencies: require('./agencies'),
 }
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(cors());
 
 app.use('/api/v1/auth', routes.auth);
 app.use('/api/v1/companies', routes.companies);
+app.use('/api/v1/agencies', routes.agencies);
 app.use('/api/v1/user', routes.user);
 
 // connection
