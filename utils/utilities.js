6c49cf4185106e231ca9c242085d489e5e4076f8
jasonhawkharris <jasonhawkharris@gmail.com>
2021-04-28 08:03:52 -0700

    Fixes 'can' functions.

--- utils/utilities.js
+++ utils/utilities.js
@@ -1,13 +1,29 @@
-const db = require('../models');
-
+/**
+ * @function canEditOrDelete()
+ * @description checks if current user is allowed to edit or delete current model
+ * @param {object} req request object
+ * @param {object} user user object
+ * @param {object} model specific instance of the model to check
+ * Would be best to declare a variable and assign the specific instance
+ * of the model that needs to be checked, then pass it in to the fxn.
+ * @returns Boolean
+ */
 const canEditOrDelete = (req, user, model) => {
     if (user.__t === 'Teammate') {
-        return false;
+        if (
+            !user.isAdmin ||
+            !user.artists.includes(model.artist)
+        ) {
+            return false;
+        }
     } else if (user.__t === 'Artist') {
         if (req.userId != model.artist) {
             return false;
         }
-    } else if (user.__t === 'Manager' || user.__t === 'Agent') {
+    } else if (
+        user.__t === 'Manager' ||
+        user.__t === 'Agent'
+    ) {
         if (!user.artists.includes(model.artist)) {
             return false;
         }
@@ -17,15 +33,30 @@ const canEditOrDelete = (req, user, model) => {
 
 
 
+/**
+ * @function canRead()
+ * @description Checks if the current user is allowed to read the current model
+ * @param {object} req request object
+ * @param {object} user current user object
+ * @param {object} model specific instance of the model to check
+ * Would be best to declare a variable and assign the specific instance
+ * of the model that needs to be checked, then pass it in to the fxn.
+ * @returns Boolean
+ */
 const canRead = (req, user, model) => {
-    if (user.__t === 'Teammate') {
-        return false;
-    } else if (user.__t === 'Artist') {
+    if (user.__t === 'Artist') {
         if (req.userId != model.artist) {
             return false;
         }
-    } else if (user.__t === 'Manager' || user.__t === 'Agent') {
-        if (!user.artists.includes(model.artist)) {
+    } else if (
+        user.__t === 'Teammate' ||
+        user.__t === 'Manager' ||
+        user.__t === 'Agent'
+    ) {
+        if (
+            !user.isAdmin ||
+            !user.artists.includes(model.artist)
+        ) {
             return false;
         }
     }
@@ -34,19 +65,36 @@ const canRead = (req, user, model) => {
 
 
 
+/**
+ * @function canCreate()
+ * @description Checks if the current user is allowed to create the current model.
+ * @param {object} req request object
+ * @param {object} user user object.
+ * @param {string} modelRef name of the model capitalized as String
+ * @returns Boolean
+ */
 const canCreate = async (req, user, modelRef) => {
     if (user.__t === 'Teammate') {
-        if (modelRef === 'Tour' || modelRef === 'Tourdate') {
-            return false;
+        if (
+            modelRef === 'Tour' ||
+            modelRef === 'Tourdate'
+        ) {
+            if (
+                !user.isAdmin ||
+                !user.artists.includes(req.body.artist)
+            ) {
+                return false;
+            }
         }
     } else if (user.__t === 'Manager' || user.__t === 'Agent') {
         if (!user.artists.includes(req.body.artist)) {
             return false;
         }
     }
-
     return true;
 }
+
+
 
 module.exports = {
     canRead,
