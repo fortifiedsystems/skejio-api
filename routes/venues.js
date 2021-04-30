506d2d304fabababc7f9b9a8e947a6bd41d6accd
jasonhawkharris <jasonhawkharris@gmail.com>
2021-04-30 10:14:40 -0700

    delete superfluous roots from venue routes.

--- routes/venues.js
+++ routes/venues.js
@@ -6,7 +6,5 @@ const authRequired = require('../middleware/authRequired');
 router.get('/', authRequired, ctrl.venues.index);
 router.get('/:id', authRequired, ctrl.venues.show);
 router.post('/', authRequired, ctrl.venues.create);
-// router.put('/:id', authRequired, ctrl.tours.update);
-// router.delete('/:id', authRequired, ctrl.tours.destroy);
 
 module.exports = router;
