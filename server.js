573016973e65a3395a72a5059930bcc8f506b209
Alexandra Kay <alexandrakay616@gmail.com>
2021-05-07 19:07:25 -0400

    added index file

--- server.js
+++ server.js
@@ -5,30 +5,28 @@ require('dotenv').config();
 
 const routes = require('./routes');
 
-const PORT = process.env.PORT || 8081;
-const app = express();
+// const PORT = process.env.PORT || 8081;
+const server = express();
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }))
-app.use(cors());
+server.use(express.json({ limit: '50mb' }));
+server.use(express.urlencoded({ limit: '50mb', extended: true }))
+server.use(cors());
 
 const authRequired = require('./middleware/authRequired');
 
-app.use('/api/v1/auth', routes.auth);
-app.use('/api/v1/mgmt', authRequired, routes.mgmt);
-app.use('/api/v1/agencies', authRequired, routes.agencies);
-app.use('/api/v1/comments', authRequired, routes.comments);
-app.use('/api/v1/tours', authRequired, routes.tours);
-app.use('/api/v1/images', authRequired, routes.images);
-app.use('/api/v1/notifications', authRequired, routes.notifications);
-app.use('/api/v1/threads', authRequired, routes.threads);
-app.use('/api/v1/tourdates', authRequired, routes.tourdates);
-app.use('/api/v1/todos', authRequired, routes.todos);
-app.use('/api/v1/user', authRequired, routes.user);
-app.use('/api/v1/venues', authRequired, routes.venues);
-app.use('/api/v1/merch', authRequired, routes.merch);
+server.use('/api/v1/auth', routes.auth);
+server.use('/api/v1/mgmt', authRequired, routes.mgmt);
+server.use('/api/v1/agencies', authRequired, routes.agencies);
+server.use('/api/v1/comments', authRequired, routes.comments);
+server.use('/api/v1/tours', authRequired, routes.tours);
+server.use('/api/v1/images', authRequired, routes.images);
+server.use('/api/v1/notifications', authRequired, routes.notifications);
+server.use('/api/v1/threads', authRequired, routes.threads);
+server.use('/api/v1/tourdates', authRequired, routes.tourdates);
+server.use('/api/v1/todos', authRequired, routes.todos);
+server.use('/api/v1/user', authRequired, routes.user);
+server.use('/api/v1/venues', authRequired, routes.venues);
+server.use('/api/v1/merch', authRequired, routes.merch);
 
 // connection
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+module.exports = server
